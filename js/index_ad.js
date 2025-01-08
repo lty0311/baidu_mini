2e98d81c8d93a60950596de43b78adf89e2fdab6
houdawei <houdawei@haohantech.net>
2025-01-08 21:53:53 +0800

    谷歌广告

--- js/index_ad.js
+++ js/index_ad.js
@@ -1,3 +1,10 @@
+// 动态添加js文件
+function loadJavaScriptFile(path) {
+  var script = document.createElement("script");
+  script.src = path;
+  document.body.appendChild(script);
+}
+
 
 // 创建 script 元素
 var script = document.createElement('script');
@@ -7,3 +14,9 @@ script.src = '/js/index_ad_right.js';
 script.type = 'text/javascript';
 // 将 script 插入到 #right_ad 元素内
 document.querySelector('#right_ad').appendChild(script);
+
+window.onload = function () {
+  setTimeout(() => {
+    loadJavaScriptFile('https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js');
+  }, 1);
+};
