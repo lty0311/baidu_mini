edf5b6d673d05ce027480bcd23d160ddcabe6f81
houdawei <houdawei@haohantech.net>
2025-01-08 22:10:13 +0800

    adad

--- js/index_ad.js
+++ js/index_ad.js
@@ -6,16 +6,7 @@ function loadJavaScriptFile(path) {
 }
 
 
-var targetDiv = document.querySelector('#right_ad');
-targetDiv.appendChild('<ins class="adsbygoogle"\n' +
-  '     style="display:block"\n' +
-  '     data-ad-format="autorelaxed"\n' +
-  '     data-ad-client="ca-pub-9761395598322566"\n' +
-  '     data-ad-slot="6980949150"></ins>');
-// 创建 script 元素
-var script_adsbygoogle = document.createElement('script');
-script_adsbygoogle.textContent = `(adsbygoogle = window.adsbygoogle || []).push({});`;
-targetDiv.appendChild(script_adsbygoogle);
+
 
 window.onload = function () {
   setTimeout(() => {
